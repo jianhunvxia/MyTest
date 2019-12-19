1c016b8d1d8cc723dcf8ee2fdf6a6ca1a6a5b3cc
jianhunvxia <zzdxwxj@163.com>
2019-12-19 19:05:06 +0800

    TestMerge输出dev+merge合并

--- src/main/java/com/qq/cases/TestMerge.java
+++ src/main/java/com/qq/cases/TestMerge.java
@@ -2,10 +2,7 @@ package com.qq.cases;
 
 public class TestMerge {
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.println("master");
-=======
         System.out.println("dev");
->>>>>>> dev
     }
 }
